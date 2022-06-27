46f8fdd6277d6eb9ec0e16c876afa180fc29c89c
Jenna Hrdlicka <hrdlickajenna@gmail.com>
2022-06-27 06:32:05 -0700

    additional seeds added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,31 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/users" do
+    user = User.all
+    user.to_json
+  end
+
+  get "/produce_offerings" do
+    produce_offering = ProduceOffering.all 
+    produce_offering.to_json
+  end
+
+  post "/produce_offerings" do
+    produce_offering = ProduceOffering.create(category: params[:category], description: params[:description], quantity: params[:quantity], user_id: params[:user_id])
+    produce_offering.to_json
+  end
+
+  patch "/produce_offerings/:id" do
+    produce_offering = ProduceOffering.find(params[:id])
+    produce_offering.update(category: params[:category], description: params[:description], quantity: params[:quantity])
+    produce_offering.to_json
+  end
+
+  delete '/produce_offerings/:id' do
+    produce_offering = ProduceOffering.find(params[:id])
+    produce_offering.destroy
+    produce_offering.to_json
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,16 @@ puts "🌱 Seeding spices..."
 u1 = User.create(name:'Ari')
 u2 = User.create(name: 'John')
 u3 = User.create(name: 'Sara')
+u4 = User.create(name: 'Phoebe')
+u5 = User.create(name: 'Joey')
 
 p1 = ProduceOffering.create(category: "Vegetables", description: "purple onions", quantity: 6, user_id: u1.id)
 p2 = ProduceOffering.create(category: "Herbs", description: "basil cuttings", quantity: 4, user_id: u2.id)
 p3 = ProduceOffering.create(category: "Fruit", description: "lemons", quantity: 22, user_id: u3.id)
+p4 = ProduceOffering.create(category: "Vegetables", description: "tomatoes", quantity: 8, user_id: u4.id)
+p5 = ProduceOffering.create(category: "Herbs", description: "bundles of thyme", quantity: 5, user_id: u5.id)
+p6 = ProduceOffering.create(category: "Flowers", description: "roses", quantity: 12, user_id: u4.id)
+p7 = ProduceOffering.create(category: "Fruit", description: "bananas", quantity: 48, user_id: u3.id)
+p8 = ProduceOffering.create(category: "Vegetables", description: "squash", quantity: 6, user_id: u2.id)
 
 puts "✅ Done seeding!"
